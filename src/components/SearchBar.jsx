2dd46985533b3e14ac8974d344bb76bd9d976384
Amitai Sopher <amitaisopher@gmail.com>
2023-06-06 12:47:18 +0300

    Add storybook with first story for SearchBar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,17 @@
 import { IoMdSearch } from 'react-icons/io'
 
 
-function SearchBar({ inputChangeHandler, submitSearchHandler, placeholderText = '', ref }) {
+function SearchBar({ inputChangeHandler, submitSearchHandler, placeholderText = '', ref, variant = 'rounded' }) {
+    const variants = {
+        "square": "rounded-none",
+        "rounded": "rounded-full"
+    }
+
     return (
-        <form data-testid='search-bar' className='h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8 mt-8'>
+        <form data-testid='search-bar' className={`h-16 bg-black/30 w-full max-w-[450px] ${variants[variant]} backdrop-blur-[32px] mb-8 mt-8`}>
             <div className="h-full relative flex items-center justify-between p-2">
                 <input ref={ref} onChange={inputChangeHandler} className="flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full" type="text" placeholder={placeholderText} />
-                <button data-testid='submit-button' onClick={submitSearchHandler} className="bg-[#1ab8ed] hover:bg-[#15abdd] w-20 h-12 rounded-full flex justify-center items-center transition">
+                <button data-testid='submit-button' onClick={submitSearchHandler} className={`bg-[#1ab8ed] hover:bg-[#15abdd] w-20 h-12 ${variants[variant]} flex justify-center items-center transition`}>
                     <IoMdSearch className="text-2xl text-white" />
                 </button>
 
---
+++ src/stories/SearchBar.stories.js
@@ -0,0 +1,20 @@
+import SearchBar from "../components/SearchBar";
+import { Button } from "./Button";
+
+export default {
+  title: "SearchBar",
+  component: SearchBar,
+};
+
+export const Square = {
+  args: {
+    variant: "square",
+    label: "Variant",
+  },
+};
+export const Rounded = {
+  args: {
+    variant: "rounded",
+    label: "Variant",
+  },
+};

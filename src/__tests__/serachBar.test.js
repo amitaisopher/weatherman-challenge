d876f9e20a188c6c7223000edb533839c55b24f6
Amitai Sopher <amitaisopher@gmail.com>
2023-06-06 10:24:11 +0300

    add more tests to SearchBar component

--- src/__tests__/serachBar.test.js
+++ src/__tests__/serachBar.test.js
@@ -1,8 +1,36 @@
 import SearchBar from "../components/SearchBar";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 
 test("Check SearchBar component renders successfully", () => {
-  render(<SearchBar />);
+  const handleSubmit = jest.fn((e) => e.preventDefault());
+  const handleChange = jest.fn();
+
+  render(
+    <SearchBar
+      inputChangeHandler={handleChange}
+      submitSearchHandler={handleSubmit}
+      placeholderText="special text goes here"
+    />
+  );
   const element = screen.getByTestId("search-bar");
   expect(element).toBeInTheDocument();
+
+  const inputField = screen.queryByPlaceholderText("special text goes here");
+  // check placeholder text is correct
+  expect(inputField).toHaveAttribute("placeholder", "special text goes here");
+  // check initial value is an empty string
+  expect(inputField).toHaveValue("");
+  // change the value of input field
+  fireEvent.change(inputField, { target: { value: "Ashdod" } });
+  // check change handler function was called
+  expect(handleChange).toBeCalledTimes(1);
+  // make sure the value of input element match to the value we changed to
+  expect(inputField.value).toBe("Ashdod");
+
+  // click on the search button
+  const submitButton = screen.getByTestId("submit-button");
+  expect(submitButton).toBeInTheDocument();
+  // check cubmit handler is called.
+  fireEvent.click(submitButton);
+  expect(handleSubmit).toBeCalledTimes(1);
 });
